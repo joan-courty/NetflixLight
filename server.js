f0829cd85a333c3b99f8d6e9f14fd06cf9987a9e
NaikoLeFou <joan.courty@ynov.com>
2026-04-14 15:27:54 +0200

    fix : reorder routes and add static file serving for src directory

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'src')));
 
+// 2. LES ROUTES API
 app.use('/auth', require('./routes/auth'));
 app.use('/movies', require('./routes/movies'));
 app.use('/user', require('./routes/user'));
-
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'src/templates/index.html'));
+});
+console.log("TMDB API Key loaded:", process.env.TMDB_API_KEY ? "Yes" : "No"); 
 app.listen(3000, () => {
   console.log('Server running on http://localhost:3000');
 });
-require('dotenv').config();
-
-console.log(process.env.TMDB_API_KEY); 
